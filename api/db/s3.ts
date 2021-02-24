dcc99cf44f8a791f1055078c121de8830f5e450f
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2021-02-24 18:49:20 +0000

    Use AA BB to test aws access keys

--- api/db/s3.ts
+++ api/db/s3.ts
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
   signatureVersion: "v4",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AA,
+  secretAccessKey: process.env.BB,
 });
 
 export default s3;
