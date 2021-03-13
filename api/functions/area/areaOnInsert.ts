e1500bd41a0ba16950defec93bf4fe3a73a99e05
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2021-03-13 09:14:32 +0000

    Add logs to logoninsert

--- api/functions/area/areaOnInsert.ts
+++ api/functions/area/areaOnInsert.ts
@@ -1,19 +1,16 @@
 import { crags } from "../../services";
 
 export const handler = async (event) => {
+  console.log("areaOnInsert");
   try {
     const promises = event.Records.map(record => {
-      console.log("areaOnInsert");
       const message = JSON.parse(record.Sns.Message);
       const { S: cragSlug } = message.dynamodb.NewImage.cragSlug;
 
-      console.log("Incrementing area count", cragSlug)
       return crags.incrementAreaCount(cragSlug);
     })
 
-    console.log("areaOnInsert awaiting")
     await Promise.all(promises)
-    console.log("areaOnInsert done")
   } catch (error) {
     console.error("Error in areaOnInsert", error)
   } finally {
--- api/functions/log/logOnInsert.ts
+++ api/functions/log/logOnInsert.ts
@@ -1,23 +1,31 @@
 import { crags, areas, routes } from "../../services";
 
 export const handler = async (event) => {
-  event.Records.forEach(record => {
-    const message = JSON.parse(record.Sns.Message);
-    const { S: hk } = message.dynamodb.NewImage.hk;
-    const { S: cragSlug } = message.dynamodb.NewImage.cragSlug;
-    const { S: areaSlug } = message.dynamodb.NewImage.areaSlug;
-    const { S: topoSlug } = message.dynamodb.NewImage.topoSlug;
-    const { S: routeSlug } = message.dynamodb.NewImage.routeSlug;
+  try {
+    const promises = [];
+    event.Records.map(record => {
+      const message = JSON.parse(record.Sns.Message);
+      const { S: hk } = message.dynamodb.NewImage.hk;
+      const { S: cragSlug } = message.dynamodb.NewImage.cragSlug;
+      const { S: areaSlug } = message.dynamodb.NewImage.areaSlug;
+      const { S: topoSlug } = message.dynamodb.NewImage.topoSlug;
+      const { S: routeSlug } = message.dynamodb.NewImage.routeSlug;
 
-    /**
-    * Two records are insertd for each log
-    * Only run analytics on one
-    */ 
-    if (hk.match(/^user#/)) {
-      crags.incrementLogCount(cragSlug)
-      areas.incrementLogCount(cragSlug, areaSlug)
-      routes.incrementLogCount(cragSlug, areaSlug, topoSlug, routeSlug)
-    }
-  })
+      /**
+      * Two records are insertd for each log
+      * Only run analytics on one
+      */ 
+      if (hk.match(/^user#/)) {
+        promises.push(crags.incrementLogCount(cragSlug));
+        promises.push(areas.incrementLogCount(cragSlug, areaSlug));
+        promises.push(routes.incrementLogCount(cragSlug, areaSlug, topoSlug, routeSlug));
+      }
+    })
+
+    await Promise.all(promises);
+    console.log("logOnInsert updates done")
+  } catch(error) {
+    console.error("Error logOnInsert", error);
+  }
 }
 
